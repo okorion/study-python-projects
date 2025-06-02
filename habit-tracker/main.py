c0bdc87d36b9794608dd8ed2d6c200aa8fce973b
okorion <ok.or.orion@gmail.com>
2025-06-02 17:12:37 +0900

    feat: pixel post 요청(잔디 심기 기능 추가)

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,11 +1,13 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-USERNAME = "hyounkyu"
+USERNAME = "okorion"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -20,3 +22,28 @@ user_params = {
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Coding Graph",
+    "unit": "commit",
+    "type": "int",
+    "color": "shibafu"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": datetime.now().strftime("%Y%m%d"),
+    "quantity": "1"
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
